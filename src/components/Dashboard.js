99eccc08391d2a1e8652c0c6c215415ad4011256
Lovro <hodak.lovro@gmail.com>
2020-12-11 17:57:14 +0100

    components, contexts, hooks

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,10 @@ import Sidebar from './Sidebar'
 
 function Dashboard({ id }) {
     return (
+        <div className='d-flex' style={{ height: '100vh'}} >
             <Sidebar id={id} />
+        </div>
+            
     )
 }
 
